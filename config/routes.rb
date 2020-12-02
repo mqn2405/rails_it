378eefc1b0c1fbad116fc1c5587f7778ed64ebdb
HSNT <hieu.jno1@gmail.com>
2020-12-02 19:49:16 +0700

    Change show user back

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,7 @@ Rails.application.routes.draw do
   get 'users', to: "static_pages#home"
   root 'static_pages#home'
   devise_for :users
-  get 'users/show'
+  # get 'users/show'
+  get 'users/show/:id', to: "users#show", as: 'users_show'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
