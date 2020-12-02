d14bea918bb08b7cd0b992396c75f52271d520c8
Ubuntu <ubuntu@ip-172-31-72-204.ec2.internal>
2020-12-02 22:11:46 +0000

    Fix sign out AWS

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,10 @@ Rails.application.routes.draw do
   get 'users', to: "static_pages#home"
   root 'static_pages#home'
   devise_for :users
+
+  devise_scope :user do  
+     get '/users/sign_out' => 'devise/sessions#destroy'     
+  end
   # get 'users/show'
   get 'users/show/:id', to: "users#show", as: 'users_show'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
