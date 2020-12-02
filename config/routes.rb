eb3183603b49ac702bddd652c725614485010b21
Quang Anh <aluong343@gmail.com>
2020-12-02 18:26:42 +0700

    Fix /users path

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,8 @@
 Rails.application.routes.draw do
   resources :restaurants
   resources :restaurant_reviews
-  get 'users/show'
+  get 'users', to: "static_pages#home"
   root 'static_pages#home'
-  get 'static_pages/home'
   devise_for :users
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
